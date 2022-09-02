ebad111bd21c264e6cc719f1e507fdcef1b4e950
Ezequiel <martinez.ezequiel42@gmail.com>
2022-09-02 01:48:33 -0700

    Imported the init connections region from FPS wave survival character controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+#region -Init Connections-
+    private bool gunisInit;
+
+    private PlayerManager playerManager;
+    private GameObject playerCap;
+    private GameObject leanPoint;
+    private GameObject playerHead;
+    private Camera playerCam;
+    private Camera gunCam;    
+
+    private PlayerController playerController; 
+
+    private GameObject WeaponUI;
+    private GameObject activeWeapon;
+
+    private WeaponUIController WUIC;
+
+    public InventoryController inventoryController;
+
+    private TMPro.TextMeshProUGUI ScoreCard;
+    private TMPro.TextMeshProUGUI AmmoCounter;
+    private TMPro.TextMeshProUGUI CST; //centerscreentext
+
+    // private Transform gunEnd;   
+    // public GameObject gun, hipPosition, ADSPosition;
+    
+
+    private CharacterController characterController;
+   
+    #endregion
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
