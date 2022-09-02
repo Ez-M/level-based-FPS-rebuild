5400cb6f328c19e73de7ffd380798c19df6ac725
Ezequiel <martinez.ezequiel42@gmail.com>
2022-09-02 02:00:20 -0700

    Added inputsystem and locked cursor

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
@@ -41,7 +42,11 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+                // Lock Cursor
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible=false;    
+
+
     }
 
     // Update is called once per frame
